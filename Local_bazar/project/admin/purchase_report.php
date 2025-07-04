2f7b8d9175a8487610888aada730b48cb110ad70
Arpit Ishan <114161202+SDEArpit@users.noreply.github.com>
2025-07-04 12:54:16 +0530

    Add files via upload

---
+++ Local_bazar/project/admin/purchase_report.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'inc/session.php';
+require_once 'inc/header.php';
+require_once 'inc/nav.php';
+?>
+
+<input type="text" id="srch1" class="form-control">
+<div id="res1"></div>
+
+<?php
+require_once 'inc/footer.php';
+?>
---
+++ Local_bazar/project/admin/stock_report.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'inc/session.php';
+require_once 'inc/header.php';
+require_once 'inc/nav.php';
+?>
+
+<input type="text" id="srch" class="form-control">
+<div id="res"></div>
+
+<?php
+require_once 'inc/footer.php';
+?>
